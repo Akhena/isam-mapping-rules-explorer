d1d26503d95e1c7e99f09d83830b83fbaf7f8845
Unknown <andre.leruitte@gmail.com>
2019-08-28 20:41:42 +0200

    MappingRules are dynamically populated
    TODO : implement IsamService

--- src/isamService.ts
+++ src/isamService.ts
@@ -1,5 +1,11 @@
+import { Environment } from "./middleware/Environment";
+
 export class IsamService {
     constructor() {
+    }
 
+    public getMappingRules(isamEnvironment: Environment): String[] {
+        // TODO call ISAM rest api
+        return ["myMappingRule.js", "myMappingRule2.js"];
     }
 }
--- src/middleware/MiddlewareTreeModel.ts
+++ src/middleware/MiddlewareTreeModel.ts
@@ -2,19 +2,21 @@ import * as vscode from 'vscode';
 
 import { Environment } from "./Environment";
 import { MiddlewareTreeNode } from "./MiddlewareTreeNode";
+import { IsamService } from '../isamService';
 
 export class MiddlewareTreeModel {
     private isamEnvironments: Environment[] = new Array();
     private datapowerEnvironments: Environment[] = new Array();
+    private isamService: IsamService = new IsamService();
 
     constructor(isamEnvs: Environment[], dpEnvs: Environment[]) {
         this.isamEnvironments = this.isamEnvironments.concat(isamEnvs);
         this.datapowerEnvironments = this.datapowerEnvironments.concat(dpEnvs);
     }
-    
+
     public get roots(): Thenable<MiddlewareTreeNode[]> {
         let mdwRootNodeArray: MiddlewareTreeNode[] = new Array();
-        
+
         const isamNode: MiddlewareTreeNode = {
             isDirectory: true,
             resource: vscode.Uri.parse("isam:ISAM")
@@ -26,31 +28,102 @@ export class MiddlewareTreeModel {
         };
         mdwRootNodeArray.push(isamNode);
 
-		return Promise.resolve(mdwRootNodeArray);
+        return Promise.resolve(mdwRootNodeArray);
     }
 
     public getChildren(node: MiddlewareTreeNode): Thenable<MiddlewareTreeNode[]> {
         let childrenNodeArray: MiddlewareTreeNode[] = new Array();
-        
+
         console.log("getChildren()");
         console.log(node);
 
-		if (node.isDirectory) {
-			switch (node.resource.path.split("/")[0].toLowerCase()) {
-				case "isam":
-					childrenNodeArray = childrenNodeArray.concat(this.createIsamEnvironmentsTreeNodes());
-					break;
-				case "datapower":
-					childrenNodeArray = childrenNodeArray.concat(this.createDatapowerEnvironmentsTreeNodes());
-					break;
-				default:
-					console.log("node.isDirectory is file");
-			}
-		}
-		else {
-			console.log("node is file");
-		}
-		return Promise.resolve(childrenNodeArray);
+        if (node.isDirectory) {
+            let splitPath = node.resource.path.split("/");
+            switch (splitPath[0].toLowerCase()) {
+                case "isam":
+                    if (splitPath.length === 1) {
+                        childrenNodeArray = childrenNodeArray.concat(this.createIsamEnvironmentsTreeNodes());
+                    } else if (splitPath.length === 2) {
+                        childrenNodeArray = childrenNodeArray.concat(this.createIsamFileTypesTreeNodes(node));
+                    } else if (splitPath.length === 3) {
+                        childrenNodeArray = childrenNodeArray.concat(this.createIsamFilesTreeNodes(node));
+                    }
+                    break;
+                case "datapower":
+                    childrenNodeArray = childrenNodeArray.concat(this.createDatapowerEnvironmentsTreeNodes());
+                    break;
+                default:
+                    console.log("node is file");
+            }
+        }
+        else {
+            console.log("node is file");
+        }
+        return Promise.resolve(childrenNodeArray);
+    }
+    private createIsamFilesTreeNodes(parentNode: MiddlewareTreeNode): MiddlewareTreeNode[] {
+        let isamFilesChildrenArray: MiddlewareTreeNode[] = new Array();
+
+        let currentEnv = this.getCurrentEnvironment(parentNode);
+
+        let splitPath = parentNode.resource.path.split("/");
+        switch (splitPath[2]) {
+            case "MAPPING-RULES":
+                // TODO
+                this.isamService.getMappingRules(currentEnv).forEach(function (file) {
+                    isamFilesChildrenArray.push({
+                        isDirectory: false,
+                        resource: vscode.Uri.parse(parentNode.resource.scheme + ":" + parentNode.resource.path + "/" + file)
+                    });
+                });
+                break;
+            case "RP-CONFIGS":
+                break;
+        }
+
+        return isamFilesChildrenArray;
+    }
+
+    private getCurrentEnvironment(parentNode: MiddlewareTreeNode): Environment {
+        let splitPath = parentNode.resource.path.split("/");
+        let foundEnv: Environment | null = null;
+        let environmentsToSearch = this.isamEnvironments;
+
+        switch (splitPath[0].toLowerCase()) {
+            case "isam":
+                environmentsToSearch = this.isamEnvironments;
+                break;
+            case "datapower":
+                environmentsToSearch = this.datapowerEnvironments;
+                break;
+        }
+
+        environmentsToSearch.forEach(function (env) {
+            if (env.name.toLowerCase() === splitPath[1].toLowerCase()) {
+                foundEnv = env;
+            }
+        });
+
+        if (foundEnv !== null) {
+            return foundEnv;
+        } else {
+            throw new Error("Unable to identify environment : " + parentNode.resource.toString);
+        }
+    }
+
+    private createIsamFileTypesTreeNodes(parentNode: MiddlewareTreeNode): MiddlewareTreeNode[] {
+        let isamFileTypesChildrenArray: MiddlewareTreeNode[] = new Array();
+
+        isamFileTypesChildrenArray.push({
+            isDirectory: true,
+            resource: vscode.Uri.parse(parentNode.resource.scheme + ":" + parentNode.resource.path + "/MAPPING-RULES")
+        });
+        isamFileTypesChildrenArray.push({
+            isDirectory: true,
+            resource: vscode.Uri.parse(parentNode.resource.scheme + ":" + parentNode.resource.path + "/RP-CONFIGS")
+        });
+
+        return isamFileTypesChildrenArray;
     }
 
     private createIsamEnvironmentsTreeNodes(): MiddlewareTreeNode[] {
@@ -60,10 +133,10 @@ export class MiddlewareTreeModel {
             environmentChildrenArray.push({
                 isDirectory: true,
                 resource: vscode.Uri.parse("isam:ISAM/" + env.name)
-             });
+            });
         });
 
-		return environmentChildrenArray;        
+        return environmentChildrenArray;
     }
 
     private createDatapowerEnvironmentsTreeNodes(): MiddlewareTreeNode[] {
@@ -73,9 +146,9 @@ export class MiddlewareTreeModel {
             environmentChildrenArray.push({
                 isDirectory: true,
                 resource: vscode.Uri.parse("isam:DATAPOWER/" + env.name)
-             });
+            });
         });
 
-		return environmentChildrenArray;        
+        return environmentChildrenArray;
     }
 }
